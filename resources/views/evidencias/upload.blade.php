38fdc704d2fada445fd3164a48a20c55c0ec72be
Alexdom123 <alexmoreno.am455@gmail.com>
2020-05-25 20:49:17 -0500

    Se uso una card para la vista

--- resources/views/evidencias/upload.blade.php
+++ resources/views/evidencias/upload.blade.php
@@ -1,13 +1,23 @@
 @extends('layout')
 
 @section('content')
-    <h4>Subir evidencias</h4>
     
-    <form method="POST" action="{{route('upload')}}" accept-charset="UTF-8" enctype="multipart/form-data">
-        {{ csrf_field() }}
-        <label for="archivo"><b>Archivo: </b></label><br>
-        <input type="file" name="archivo" required>
-        <br><br>
-        <input class="btn btn-success" type="submit" value="Enviar" >
-      </form>
+    
+    <div class="col-md-7">
+      <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Subir evidencias</h5>
+            <form method="POST" action="{{route('upload')}}" accept-charset="UTF-8" enctype="multipart/form-data">
+              {{ csrf_field() }}
+              <label for="archivo"><b>Archivo: </b></label><br>
+              <input type="file" name="archivo" required>
+              <br><br>
+              <input class="btn btn-success" type="submit" value="Enviar" >
+            </form>
+  
+          </div>
+      </div>
+  </div>
+
+
 @endsection
